5fc19df7a8caa93e442d85d706669b88a05fd4b0
Paulo <paulo@philippinedev.com>
2021-09-17 17:33:10 +0800

    add click events feature

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
       author: 'Brandon Sanderson',
       age: 45
     }
+  },
+  methods: {
+    changeTitle(title) {
+      // this.title = 'Words of Radiance'
+      this.title = title
+    }
   }
 })
 
